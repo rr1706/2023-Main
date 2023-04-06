55dbba41c41606e01abcaca3e8be2ca9a37863c7
FRC1706 <124755401+FRC1706@users.noreply.github.com>
2023-04-06 17:37:04 -0500

    Remove photonvision

--- src/main/java/frc/robot/utilities/AutoBuilder.java
+++ src/main/java/frc/robot/utilities/AutoBuilder.java
@@ -17,25 +17,9 @@ import frc.robot.subsystems.Drivetrain;
 
 public class AutoBuilder {
     private final Drivetrain m_drivetrain;
-    private final SwerveAutoBuilder m_defaultBuilder;
 
-    public AutoBuilder(Drivetrain drivetrain, PIDConstants translationConstants, PIDConstants rotationConstants, HashMap<String, Command> events) {
+    public AutoBuilder(String name, Drivetrain drivetrain, PIDConstants translationConstants, PIDConstants rotationConstants) {
         m_drivetrain = drivetrain;
-        m_defaultBuilder = new SwerveAutoBuilder(m_drivetrain::getPose, m_drivetrain::resetOdometry, DriveConstants.kDriveKinematics, translationConstants, rotationConstants, m_drivetrain::setModuleStates, events, true, m_drivetrain);
-    }
-
-    public CommandBase fullAuto(String name) {
-        List<PathPlannerTrajectory> defaultTrajectories = PathPlanner.loadPathGroup(name, new PathConstraints(DriveConstants.kMaxSpeedMetersPerSecond, DriveConstants.kMaxAcceleration));
-        PathConstraints firstConstraints = new PathConstraints(Math.min(Math.max(defaultTrajectories.get(0).getInitialState().velocityMetersPerSecond, defaultTrajectories.get(0).getEndState().velocityMetersPerSecond), DriveConstants.kMaxSpeedMetersPerSecond), DriveConstants.kMaxAcceleration);
-        PathConstraints[] correctedConstraints = new PathConstraints[defaultTrajectories.size() - 1];
-        for (int i = 1; i < defaultTrajectories.size() - 1; i++) {
-            correctedConstraints[i] = new PathConstraints(Math.min(Math.max(defaultTrajectories.get(i).getInitialState().velocityMetersPerSecond, defaultTrajectories.get(i).getEndState().velocityMetersPerSecond), DriveConstants.kMaxSpeedMetersPerSecond), DriveConstants.kMaxAcceleration);
-        }
-        try {
-            return m_defaultBuilder.fullAuto(PathPlanner.loadPathGroup(name, firstConstraints, correctedConstraints));
-        } catch (NullPointerException e) {
-            return m_defaultBuilder.fullAuto(PathPlanner.loadPathGroup(name, firstConstraints));
-        }
     }
 
 }
